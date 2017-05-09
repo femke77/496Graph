398df21bbd97ac1a2c74e7c9e2b8f245bebfead5
femke77 <megan.meyers.388@my.csun.edu>
2017-05-08 17:55:34 -0700

    project is completed - needs more testing though

--- src/project/Graph.java
+++ src/project/Graph.java
@@ -1,3 +1,7 @@
+// Megan Meyers
+// 496 Project 3
+// 5/11/19
+
 package project;
 
 import java.io.*;
@@ -32,7 +36,7 @@ public class Graph {
 			}
 			scan.close();
 		} catch (FileNotFoundException e) {
-			System.out.println("File not found exception");
+			System.out.println("File not found");
 			e.printStackTrace();
 		}
 	}
@@ -63,7 +67,6 @@ public class Graph {
 		for (int i = 0; i < get_nVertices(); i++) {
 			System.out.println("vertex = " + i + "   " + adjList[i].toString());
 		}
-
 	}
 
 	public int get_nVertices() {
@@ -79,38 +82,34 @@ public class Graph {
 	}
 
 	
-	
-	
-	
 	public void dijkstraShortestPaths(int start){
 		 
 		 PriorityQueue<PQNode> pq = new PriorityQueue<PQNode>();
 		 int[] distance = new int[nVertices];
-		 int[] parent = new int[nVertices];
-		 int[] s = new int[nVertices];		 
+		 int[] parent = new int[nVertices];					 
+		 int[] s = new int[nVertices];		 				 //is a vertex in the cloud? if yes 1, if not 0
 		 for (int i = 0; i < nVertices; i++){
 			 distance[i] = 1000000;
 			 parent[i] = -1;
 			 s[i] = 0;
 			 if(i == start)
-				 pq.add(new PQNode(i, 0));					 //make sure the start node gets marked with zero distance	 
+				 pq.add(new PQNode(i, 0));					 //make sure the start node gets marked with 0 distance	 
 			 else
 				 pq.add(new PQNode(i, distance[i]));			 			 
 		 }
-		 distance[start] = 0;	
-		 
+		 distance[start] = 0;			 
 		 while (!pq.isEmpty()){
-			 PQNode temp = pq.poll();  						 //remove the min node, will get start vertex first
+			 PQNode temp = pq.poll();  						 //remove the min node - gets start vertex first (0 distance)
 			 int u = temp.vertex;
-			 s[u] = 1;  			 												 
+			 s[u] = 1;  			 						 //add vertex u to the cloud					 
 			 for (int i = 0; i < adjList[u].size(); i++){	 //look at each edge that originates from vertex u
 				 int z = adjList[u].get(i).vertex2;
 				 int w = adjList[u].get(i).weight;
-				 if (s[z] == 0){ 							 //check if vertex is already included
-					 if (distance[z] > distance[u] + w){
+				 if (s[z] == 0){ 							 //check that vertex z is not already in the cloud
+					 if (distance[z] > distance[u] + w){	 //edge relaxation- shrinks the upper bound on distance
 						 distance[z] = distance[u] + w;
-						 parent[z] = u;
-						 pq.add(new PQNode(z,distance[z]));  //update the distance						 
+						 parent[z] = u;						 
+						 pq.add(new PQNode(z,distance[z]));  //update the distance by replacing the node						 
 					 }					 
 				 }				 
 			 }			 
@@ -121,12 +120,13 @@ public class Graph {
 	 
 	
     //Kruskal's algorithm for MST implements clusters as an array of linked-lists with an addition names array that memoizes locations of 
-	//vertices. Integer objects are aliases for vertices in the linked-lists. A java PQ allows consideration by increasing weight - 
+	//vertices. Integer objects are aliases for vertices in the linked-lists. A java PQ allows for consideration by increasing weight - 
 	//ties are handled arbitrarily. If edge weights are not unique, different MSTs MAY result. 
+	
+	@SuppressWarnings("unchecked")
 	public Graph kruskalMST() {
 		
 		Graph mst = new Graph(nVertices);		
-		@SuppressWarnings("unchecked")
 		LinkedList<Integer>[] cluster = new LinkedList[nVertices];
         int[] names = new int[nVertices];
 		for (int i = 0; i < nVertices; i++) {
@@ -143,15 +143,15 @@ public class Graph {
 			EdgeNode temp = pq.poll();
 			u = temp.vertex1;
 			v = temp.vertex2;
-			indexu = names[u];  									//check the names array to find location of u and v
+			indexu = names[u];  										//check the names array to find location of u and v
 			indexv = names[v];		
-			if (names[indexu] != names[indexv]) {   				//if these values != then they reside in different clusters
+			if (names[indexu] != names[indexv]) {   					//if these values != then they reside in different clusters
 				mst.addEdge(u, v, temp.weight);				
 				if (cluster[indexu].size() > cluster[indexv].size()) {
 					for (int i = 0; i < cluster[indexv].size(); i++) {
-						int vertex = cluster[indexv].remove(i); 	//remove a vertex from old cluster
-						cluster[indexu].add(vertex);				//add it to the new cluster
-						names[vertex] = indexu; 					//go to its names memo and update its location
+						int vertex = cluster[indexv].remove(i); 		//remove a vertex from old cluster
+						cluster[indexu].add(vertex);					//add it to the new cluster
+						names[vertex] = indexu; 						//go to its names memo and update its location
 					}
 				} else {
 					for (int i = 0; i < cluster[indexu].size(); i++) {
@@ -165,11 +165,11 @@ public class Graph {
 		return mst;
 	}
 	
-	//TODO - spanning tree for connected/null if not connected
 	public Graph dfsTraversal(int start){
 		
-		Graph tree = new Graph(nVertices);
-		int components = 0;
+		boolean connected = true;
+		int components;
+		Graph tree = new Graph(nVertices);		
 		ArrayList<Integer> order = new ArrayList<Integer>();
 	    int[] visit = new int[nVertices];
 	    for (int i = 0; i < nVertices; i++){
@@ -177,49 +177,57 @@ public class Graph {
 	    }
 		dfsHelper(start, visit, order);	
         components = 1;
-		boolean connected = true;
-		for(int i = 0; i < visit.length; i++){
+		for (int i = 0; i < visit.length; i++){
 			if (visit[i] == 0)
 				connected = false;
 		}				
-		if(connected){
+		if (connected){
 			System.out.println("Graph is connected");
-			//TODO spanning tree
-			
-		}else {
-			System.out.println("Graph is unconnected. Running dfsTraversal on remaining vertices");			
+			tree = spanFromDFS(tree);						
+		} else {
+			System.out.println("Graph is not connected. Running dfsTraversal on remaining vertices");		
+			tree = null;
 			for (int i = 0; i < visit.length; i++){
 				if (visit[i] == 0){
-					dfsHelper(i, visit, order);
 					components += 1;
+					dfsHelper(i, visit, order);
 				}
 			}
 		}
-		System.out.println("Number of components: "+ components+"\nCycle: "+cycle);
-		System.out.println("Order visited: "+ order.toString());
+		System.out.println("Order nodes were visited: "+ order.toString());
+		System.out.println("Number of components: "+ components + "\nHas cycle? "+ cycle);		
 		return tree;
 	}
-		
+	
 	private void dfsHelper(int s, int[] visited, ArrayList<Integer> orders)	{
 		
-		visited[s] = 1; 								//mark vertex visited
-		orders.add(s);
-		for(int i = 0; i < adjList[s].size(); i++){
-			if (!adjList[s].get(i).explored){  			//if edge is unexplored
-				int w = adjList[s].get(i).vertex2;  	//let w = the end vertex
-				if(visited[w] == 0){ 					//if the vertex is unexplored
-					adjList[s].get(i).back = false; 	//not back = discovery
-					dfsHelper(w, visited, orders);
-				}else{
+		visited[s] = 1; 								   //mark vertex visited with 1 
+		orders.add(s);									   //save the order that vertices are visited
+		for (int i = 0; i < adjList[s].size(); i++){
+			if (!adjList[s].get(i).explored){  			   //if edge is unexplored
+				int w = adjList[s].get(i).vertex2;  	   //let w = the end vertex
+				if (visited[w] == 0){ 					   //if the vertex is unexplored
+					adjList[s].get(i).back = false; 	   //not back = discovery
+					dfsHelper(w, visited, orders);					  
+				} else {
 					adjList[s].get(i).back = true;
-					cycle = true;						//back edges indicate cycles
+					cycle = true;						   //back edges indicate cycles
 				}
 			}			
 		}		
 	}
+	
+	private Graph spanFromDFS(Graph t){
 		
-	
-	
+		for (int i = 0; i < nVertices; i++){
+			for (int j = 0; j < adjList[i].size(); j++){	
+				if (!adjList[i].get(j).back){				
+					t.addEdge(adjList[i].get(j).vertex1, adjList[i].get(j).vertex2, adjList[i].get(j).weight);
+				}
+			}			
+		}		
+		return t;
+	}
 }// end class Graph
 
 
@@ -229,8 +237,8 @@ class EdgeNode implements Comparable<EdgeNode> {
 	int vertex1;
 	int vertex2;
 	int weight;
-	boolean back = false;			 //not back is discovery -DFS
-	boolean explored = false;		 //edge explored or not -DFS
+	boolean back;			 //for DFS - back = false means edge is discovery
+	boolean explored;		 //for DFS 
     
 	
 	public EdgeNode(int v1, int v2, int w) {
@@ -271,3 +279,7 @@ class PQNode implements Comparable<PQNode> {
 	}
 
 }// end class PQNode
+
+//PERSONAL: anything passed by reference will update and be usable in original after recursive helper method finishes, 
+//but a primitive type can only be passed? when i tried to pass int cycle to helper, change it in helper, and use it again 
+//in original method, it failed. but my arrays work fine. check on this later. 
